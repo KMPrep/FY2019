9a9edde6b34ff1c97cc83076edf8ad997c568fb6
Karthick Mahalingam <mahakarthick@gmail.com>
2018-06-10 16:34:20 -0700

    adding problem1

---
+++ backtracking/BackTracking/Problem1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BackTracking {
+    /*
+    Generate all strings of n bits 
+    */
+    public class Problem1 {
+
+        public static char[] charArray;
+
+        public static void Solution(int n) {
+            charArray = new char[n];
+            Binary(n);
+        }
+
+        private static void Binary(int n) {
+            if (n < 1) {
+                Console.WriteLine(new String(charArray));
+                return;
+            }
+
+            charArray[n - 1] = '0';
+            Binary(n - 1);
+            charArray[n - 1] = '1';
+            Binary(n - 1);
+
+        }
+    }
+}
---
+++ backtracking/BackTracking/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BackTracking {
+    class Program {
+        static void Main(string[] args) {
+            Problem1.Solution(2);
+        }
+    }
+}
